5100f75df7206d1a70560a2b18c036f17b7e578c
Mike Levan <mlevan1992@gmail.com>
2019-01-09 17:07:02 -0500

    Retrieve information about your Redis cluster in a tuple output.

---
+++ get_redis_cluster_info.py
@@ -0,0 +1,26 @@
+import boto3
+import sys
+
+def describe_redis_url(region, accessKey, secretKey, redisClusterID):
+    try:
+        redis = boto3.client('elasticache',
+                             region_name=region,
+                             aws_access_key_id=accessKey,
+                             aws_secret_access_key=secretKey)
+
+        redis_url = redis.describe_replication_groups(ReplicationGroupId=redisClusterID)
+
+        for cluster in redis_url['ReplicationGroups']:
+            for i in cluster.items():
+                print(i)
+                
+    except ValueError:
+        print('Please check your access key, secret key, and redis cluster ID to confirm all is correct')
+
+
+region = sys.argv[1]
+accessKey = sys.argv[2]
+secretKey = sys.argv[3]
+redisClusterID = sys.argv[4]
+
+describe_redis_url(region, accessKey, secretKey, redisClusterID)
